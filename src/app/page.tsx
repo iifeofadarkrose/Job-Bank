bf5b5e4b8567a0c7d37c0a6f7a1088ef6d3aa594
Viacheslav Babenko <vacheslav69moss@gmail.com>
2024-08-18 15:42:20 -0400

    Add FullAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 import Hero from "./Components/Hero";
 import Jobs from "./Components/Jobs";
+import Link from 'next/link';
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
   return (
    <div>
     <Hero/>
